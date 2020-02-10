47030ba7c736af29e6403589e193f3d5258190b5
Shogo Murakami <shogomurakami@shibanonoMacBook-puro.local>
2020-02-10 23:31:50 +0900

    add meta

--- creator.py
+++ creator.py
@@ -18,7 +18,7 @@ index_str = """<!doctype html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="../static/main.css">
 
