109519985d185b3eef2b0dced84768eaea5a4ed8
ShogoMurakami <shogopc1@gmail.com>
2020-12-23 22:19:23 +0900

    Update bulma

--- creator.py
+++ creator.py
@@ -108,10 +108,10 @@ index_bulma_str = """<!doctype html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="../static/main.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
 
     <title>""" + project_name + """</title>
 
